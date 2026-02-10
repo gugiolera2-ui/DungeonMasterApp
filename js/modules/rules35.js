26f2d99af9dc0e5c6e1a75b13aac23bb3980d91c
gugiolera2-ui <gugiolera2@gmail.com>
2026-02-10 13:17:32 +0100

    Delete js/modules/rules35.js

--- js/modules/rules35.js
+++
@@ -1,8 +0,0 @@
-export const Rules35 = {
-    getModifier: (score) => Math.floor((score - 10) / 2),
-    
-    calculateAC: (data) => {
-        // Base 10 + Dex + Armor + Shield + Natural + Size
-        return 10 + (data.dexMod || 0) + (data.armor || 0) + (data.shield || 0);
-    }
-};
